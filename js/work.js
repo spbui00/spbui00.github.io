56ddaaface9b50fc7aeee6f724e5447c62e67b8e
Thai Ha Bui <sp.bui00@sparkenv.com>
2023-07-18 13:11:00 +0200

    added json projects

--- js/work.js
+++ js/work.js
@@ -1,3 +1,42 @@
 export function handleWork() {
-  console.log('handling work page!');
+  const projectsContainer = document.querySelector('#projects .items');
+
+  fetch('../assets/data/projects.json')
+    .then((response) => response.json())
+    .then((data) => {
+      const projects = data.projects;
+
+      // TODO: add sorting and filtering
+
+      projects.forEach((project) => {
+        const projectItem = document.createElement('div');
+        projectItem.classList.add('item-project');
+        const itemName = document.createElement('div');
+        itemName.classList.add('item-name');
+
+        if (projects.icon) {
+          const itemIcon = document.createElement('div');
+          itemIcon.classList.add('item-icon');
+          fetch(`../assets/icons/${project.icon}.svg`)
+            .then((response) => response.text())
+            .then((svg) => {
+              itemIcon.innerHTML = svg;
+              itemName.appendChild(itemIcon);
+            })
+            .catch((error) => {
+              console.error(error);
+            });
+        }
+        
+        itemName.innerHTML += project.name;
+        projectItem.appendChild(itemName);
+
+        const itemDescription = document.createElement('div'); 
+        itemDescription.classList.add('item-description');
+        itemDescription.innerHTML = project.description;
+        projectItem.appendChild(itemDescription);
+
+        
+      });
+    });
 }
